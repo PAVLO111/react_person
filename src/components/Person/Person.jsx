deda162b741e88b8476b0e901f0d1b01f0d9c76b
pavlo <nadazasada@gmail.com>
2026-09-24 12:29:03 +0300

    created age and name

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,26 @@
-// export const Person = ({ person }) => ();
+// import cn from 'classnames';
+
+export const Person = ({ person }) => {
+  const { age, name, isMarried, partnerName } = person;
+  // let text =
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">My name is {name}</h2>
+
+      {age && (
+        <p className="Person__age">I am {age}</p>
+      )}
+
+      <p>{partnerName} is my </p>
+
+      {/* {isMarried && partnerName && (
+        <p className="Person__partner">{partnerName} is my { sex === 'm' ? 'wife' : 'husband' }</p>
+      )} */}
+
+      {/* <p className="Person__partner"></p> */}
+
+    </section>
+  )
+}
+
