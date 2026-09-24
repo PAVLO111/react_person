755981cacb45b11303e6306f56f63fc4f9ce99b2
pavlo <nadazasada@gmail.com>
2026-09-24 12:40:18 +0300

    created all components

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,26 +1,26 @@
 // import cn from 'classnames';
 
 export const Person = ({ person }) => {
-  const { age, name, isMarried, partnerName } = person;
+  const { age, name, sex, isMarried, partnerName } = person;
   // let text =
 
   return (
     <section className="Person">
       <h2 className="Person__name">My name is {name}</h2>
 
-      {age && (
-        <p className="Person__age">I am {age}</p>
-      )}
+      {age && <p className="Person__age">I am {age}</p>}
 
-      <p>{partnerName} is my </p>
+      {/* <p>{partnerName} is my </p> */}
 
       {/* {isMarried && partnerName && (
         <p className="Person__partner">{partnerName} is my { sex === 'm' ? 'wife' : 'husband' }</p>
       )} */}
 
-      {/* <p className="Person__partner"></p> */}
-
+      <p className="Person__partner">
+        { isMarried && partnerName
+        ?  `${partnerName} is my ${sex === 'm' ? 'wife' : 'husband'}`
+        : 'Im not married'}
+      </p>
     </section>
-  )
-}
-
+  );
+};
