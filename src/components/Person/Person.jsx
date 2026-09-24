120d9028e57c0070582f05fd321e0101c4906ed9
pavlo <nadazasada@gmail.com>
2026-09-24 12:53:40 +0300

    prepare to request

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -2,7 +2,6 @@
 
 export const Person = ({ person }) => {
   const { age, name, sex, isMarried, partnerName } = person;
-  // let text =
 
   return (
     <section className="Person">
@@ -10,16 +9,10 @@ export const Person = ({ person }) => {
 
       {age && <p className="Person__age">I am {age}</p>}
 
-      {/* <p>{partnerName} is my </p> */}
-
-      {/* {isMarried && partnerName && (
-        <p className="Person__partner">{partnerName} is my { sex === 'm' ? 'wife' : 'husband' }</p>
-      )} */}
-
       <p className="Person__partner">
-        { isMarried && partnerName
-        ?  `${partnerName} is my ${sex === 'm' ? 'wife' : 'husband'}`
-        : 'Im not married'}
+        {isMarried && partnerName
+          ? `${partnerName} is my ${sex === 'm' ? 'wife' : 'husband'}`
+          : 'Im not married'}
       </p>
     </section>
   );
